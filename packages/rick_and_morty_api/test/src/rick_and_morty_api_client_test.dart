55b7dbb4d03eaa3ef64390524cc0d7bd46f359ff
Miguel Yurivilca <miguel.yurivilca.16@gmail.com>
2022-08-26 12:34:07 -0500

    test: rick and morty api client constructor error

--- packages/rick_and_morty_api/test/src/rick_and_morty_api_client_test.dart
+++ packages/rick_and_morty_api/test/src/rick_and_morty_api_client_test.dart
@@ -1,11 +1,29 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:rick_and_morty_api/rick_and_morty_api.dart';
+
+class MockDio extends Mock implements Dio {}
+
+class MockResponse<T> extends Mock implements Response<T> {}
 
 void main() {
   group('RickAndMortyApiClient', () {
-    test('can be instantiated', () {
-      expect(RickAndMortyApiClient(), isNotNull);
+    late Dio dio;
+    late RickAndMortyApiClient rickAndMortyApiClient;
+
+    setUp(() {
+      dio = MockDio();
+      // rickAndMortyApiClient = RickAndMortyApiClient(httpClient: dio);
     });
+
+    group('constructor', () {
+      test('does not require a dio client', () {
+        expect(RickAndMortyApiClient(), isNotNull);
+      });
+    });
+
+    group('getCharacters', () {});
   });
 }
